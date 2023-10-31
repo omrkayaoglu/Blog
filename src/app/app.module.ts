2776f5b63caf197bf5b17ba508644a669fa1daa6
omrkayaoglu <omer.70942@gmail.com>
2023-10-31 15:21:53 +0300

    finish project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {HttpClientModule } from '@angular/common/http';
+import { BaseService } from './services/base.service';
+import { BlogDialogComponent } from './home/blog-dialog/blog-dialog.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input'
+import {MatFormFieldModule} from '@angular/material/form-field'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    MatCardModule,
+    MatButtonModule,
+    HttpClientModule,
+    MatDialogModule,
+    MatDividerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
-  providers: [],
+  providers: [BaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/home/blog-dialog/blog-dialog.component.ts
@@ -0,0 +1,66 @@
+import { Component, Inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef,MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { BlogService } from 'src/app/services/blog.service';
+import { CommentService } from 'src/app/services/comment.service';
+
+@Component({
+  selector: 'app-blog-dialog',
+  templateUrl: './blog-dialog.component.html',
+  styleUrls: ['./blog-dialog.component.css']
+})
+export class BlogDialogComponent {
+  isUpdate:boolean=false;
+  imageUrl:string="";
+  title:string="";
+  body:string="";
+
+  commentData:any;
+
+  form=new FormGroup({
+    title:new FormControl(null,[Validators.required]),
+    body:new FormControl(null,[Validators.required])
+  });
+
+  blogElement:any;
+
+  constructor(private blogService:BlogService,private commentService:CommentService,@Inject(MAT_DIALOG_DATA) private data: any, private dialogRef:MatDialogRef<BlogDialogComponent>){
+    if(data.isUpdate){
+      this.isUpdate=true;
+      this.form.patchValue({
+        title:data.blog.title,
+        body:data.blog.body
+      });
+      this.blogElement=data.blog;
+    }
+    else{
+      this.imageUrl=data.blog.imageId.toString();
+      this.title=data.blog.title;
+      this.body=data.blog.body;
+    }
+  }
+
+  ngOnInit():void{
+    this.commentService.getComments().subscribe((res)=>{
+      this.commentData=res.filter((x: { postId: any; })=>x.postId==this.data.blog.id)
+    })
+  }
+
+  close(){
+    this.dialogRef.close();
+  }
+
+  onSubmit(){
+    const request={
+      title:this.form.get('title')?.value,
+      body:this.form.get('body')?.value,
+      imageId:this.data.blog.imageId,
+      userId:this.data.blog.userId
+    };
+    this.blogService.updatePost(this.data.blog.id,request).subscribe((res)=>{
+      this.blogElement.title=res.title;
+      this.blogElement.body=res.body;
+      this.close();
+    })
+  }
+}
